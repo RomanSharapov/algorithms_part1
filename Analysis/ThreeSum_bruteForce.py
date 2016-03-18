bf62e797d8662a8e6b3bdfba261d9ed3386108d7
Roman Sharapov <info@romansharapov.com>
2016-03-18 15:01:00 +0300

    Brute-force 3-Sum algorithm speed estimated

--- Analysis/ThreeSum_bruteForce.py
+++ Analysis/ThreeSum_bruteForce.py
@@ -16,15 +16,17 @@
 
 def three_sum(a):
     """
-    3-Sum brute-force algorithm
+    3-Sum brute-force algorithm O(N^3)
+    It seems that speed is T(N) = 2.5E-8 * N^3
     Given N distinct integers, how many triples sum to exactly zero?
     :param a: list of integers
     :return count: how many triples sum to exactly zero
     """
     N = len(a)
     count = 0
-    for i in range(N):
-        for j in range(i+1, N):
+
+    for i in range(N-2):
+        for j in range(i+1, N-1):
             for k in range(j+1, N):
                 if a[i] + a[j] + a[k] == 0:
                     count += 1
